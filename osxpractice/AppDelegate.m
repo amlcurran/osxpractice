32e0c3208ddb1c73b51c6268d7124554b47e1e84
Alex Curran <aml.curran@gmail.com>
2013-12-18 22:44:23 +0000

    App loads data from OxBump and loads it into a label and image view

--- osxpractice/AppDelegate.m
+++ osxpractice/AppDelegate.m
@@ -14,5 +14,28 @@
 {
     // Insert code here to initialize your application
 }
+- (void)parseWeatherData:(NSData *)data {
+    NSString *stringResult = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSArray *lines = [stringResult componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
+    NSString *imageUrl = [lines objectAtIndex:3];
+    NSURLRequest *imageRequest = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:imageUrl]];
+    [NSURLConnection sendAsynchronousRequest:imageRequest queue:[NSOperationQueue currentQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error)
+     {
+         NSImage *image = [[NSImage alloc] initWithData:data];
+         [_weatherImage setImage:image];
+         
+     }];
+    [_result setStringValue:stringResult];
+}
+
+- (IBAction)buttonClick:(id)sender {
+    // http://www.feathersquare.com/data/OxWeather.dat
+    
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://www.feathersquare.com/data/OxWeather.dat"]];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue currentQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error)
+     {
+         [self parseWeatherData:data];
+     }];
+}
 
 @end
